57084e871bb330323246da829ea09c8e20c2463a
manvigit84 <srimanvi84@gmail.com>
2026-07-25 23:54:00 +0530

    learned how to compare current node with next node

---
+++ LinkedList/83.RemoveDuplicatesfromsortedlist.cpp
@@ -0,0 +1,20 @@
+lass Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(head == nullptr)
+        return head;
+
+        ListNode* curr = head;
+
+        while(curr->next != nullptr){
+
+            if(curr->val == curr->next->val){
+                curr->next = curr->next->next;
+            }
+            else{
+                curr = curr->next;
+            }
+        }
+        return head;
+    }
+};
