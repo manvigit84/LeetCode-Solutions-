826aa4a00d34664ea293437be995107b823e83da
manvigit84 <srimanvi84@gmail.com>
2026-08-16 00:00:53 +0530

    solved postorder

---
+++ Trees/biarytreepostorder.cpp
@@ -0,0 +1,26 @@
+lass Solution {
+public:
+       vector<int> result;
+
+        void postorder(TreeNode* root){
+            
+            if(root == nullptr)
+            return ;
+            
+             postorder(root->left);
+             postorder(root->right); 
+
+           
+
+            result.push_back(root -> val);
+
+           
+
+        }
+    vector<int> postorderTraversal(TreeNode* root) {
+
+        postorder(root);
+
+        return result;
+    }
+};
