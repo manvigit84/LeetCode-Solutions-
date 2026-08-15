fb904a1682bdff024ddf5358d46d7d82c4cbba6a
manvigit84 <srimanvi84@gmail.com>
2026-08-15 23:33:12 +0530

    more hashing problems

---
+++ Hashing/2351.FirstLetterToAppearTwice.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    char repeatedCharacter(string s) {
+        
+        unordered_set<char> st;
+
+        for(char c : s){
+            if(st.count(c)){
+                return c;
+            }
+                st.insert(c);
+            
+        }
+        return ' ';
+    }
+};
