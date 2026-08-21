c5f11aa1c24cd73f4f33e8e5e40c4f6adf032f0a
manvigit84 <srimanvi84@gmail.com>
2026-08-21 12:33:55 +0530

    Solved Leetcode 1046 - Last Stone Weight (Heap Problems)

---
+++ Heap/1046.LastStoneWeight.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    priority_queue<int> pq;
+    
+    int lastStoneWeight(vector<int>& stones) {
+       for(int x : stones)
+       {
+        pq.push(x);
+       }
+
+       while(pq.size() > 1)
+       {
+       
+       int first = pq.top();
+       pq.pop();
+
+       int second = pq.top();
+       pq.pop();
+
+        if(first != second)
+        {
+            pq.push(first - second);
+        }
+       }
+       
+       if(pq.empty())
+        return 0;
+
+       return pq.top();
+       }
+};
