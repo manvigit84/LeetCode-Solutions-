84c9137bc882e36f978169dd66a24254dcbeeb6c
manvigit84 <srimanvi84@gmail.com>
2026-09-11 12:02:29 +0530

    solved leetcode 1672 - Richest Customer Wealth

---
+++ Arrays/1672.RichestCustomerWealth.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int n = accounts.size();
+        int m = accounts[0].size();
+        int wealth = 0;
+       
+
+        for(int i = 0; i<n; i++){
+             int cust1 = 0;
+
+            for(int j = 0; j<m; j++){
+          cust1 += accounts[i][j];
+            }
+        
+        wealth = max(wealth , cust1);
+        }
+        return wealth;
+    }
+};
